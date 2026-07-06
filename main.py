5ee5c90b963a630cbf74860397706bf3a3f5a6b5
Mustafa_Khan <121564051+Mustafa475689@users.noreply.github.com>
2026-07-06 19:49:52 +0500

    Refactor main.py to define Bank class
    Refactor main.py to define Bank class

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+
+class Bank:
+    def createaccount(self):
+        pass
+
+    
+user = Bank()
+
 print("Press 1 for Creating an Account")
 print("Press 2 for Deposite the money in the Bank")
 print("Press 3 for withdraw the money")
 print("Press 4 for Deteils")
 print("Press 5 to Update the Details")
 print("Press 6 for Delete the Account")
+
+check = int(input("Tell your response here: "))
+
+if  check == 1:
+    user.createaccount()
